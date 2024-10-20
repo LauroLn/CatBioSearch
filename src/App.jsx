1cbdef0b11cb143cdfcb69915fa6092a82742d32
Henry-AF <henryfernandes001@gmail.com>
2024-10-20 13:49:38 -0300

    Login frontend

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Login from './Components/login/login';
+import Acess from './Components/primeiroAcesso/acess'
+import './App.css'
+
+function App() {
+  return(
+
+    <Login/>
+    //<Acess/>
+  );
+}
+
+export default App
